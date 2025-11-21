74097a911c5c29eddc6e42cdec67237b990e9e19
ぷとん <areas.ruler@gmail.com>
2025-11-22 01:43:46 +0900

    一時的にfetchの方法を変更

--- dist/cjs/packages/ueuses/fetchUeuses.js
+++ dist/cjs/packages/ueuses/fetchUeuses.js
@@ -6,16 +6,28 @@ async function fetchUeuses(userAgent, limit, page) {
     if (!this.me) {
         throw new error_1.UwuzuError("ログインコマンドを実行していません", error_1.UwuzuErrorCode.UWUZU_LOGIN_03);
     }
-    const data = await fetch(`https://${this.domain}/api/ueuse`, {
+    const data = await fetch(`https://${this.domain}/api/ueuse?token=${this.token}`, {
         method: "POST",
         headers: {
             "User-Agent": userAgent,
         },
         body: JSON.stringify({
-            token: this.token,
             limit: limit,
             page: page,
         }),
     }).then((res) => res.json());
+    /*
+    const data = await fetch(`https://${this.domain}/api/ueuse`, {
+      method: "POST",
+      headers: {
+        "User-Agent": userAgent,
+      },
+      body: JSON.stringify({
+        token: this.token,
+        limit: limit,
+        page: page,
+      }),
+    }).then((res) => res.json() as Promise<UeuseResponse[]>);
+    */
     return data;
 }
--- dist/esm/packages/ueuses/fetchUeuses.js
+++ dist/esm/packages/ueuses/fetchUeuses.js
@@ -3,16 +3,28 @@ export async function fetchUeuses(userAgent, limit, page) {
     if (!this.me) {
         throw new UwuzuError("ログインコマンドを実行していません", UwuzuErrorCode.UWUZU_LOGIN_03);
     }
-    const data = await fetch(`https://${this.domain}/api/ueuse`, {
+    const data = await fetch(`https://${this.domain}/api/ueuse?token=${this.token}`, {
         method: "POST",
         headers: {
             "User-Agent": userAgent,
         },
         body: JSON.stringify({
-            token: this.token,
             limit: limit,
             page: page,
         }),
     }).then((res) => res.json());
+    /*
+    const data = await fetch(`https://${this.domain}/api/ueuse`, {
+      method: "POST",
+      headers: {
+        "User-Agent": userAgent,
+      },
+      body: JSON.stringify({
+        token: this.token,
+        limit: limit,
+        page: page,
+      }),
+    }).then((res) => res.json() as Promise<UeuseResponse[]>);
+    */
     return data;
 }
